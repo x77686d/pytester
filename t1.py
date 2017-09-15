fb1e24e0516bca1c2893fd6bc1acf1b599c4ad0c
William H. Mitchell <whm@cs.arizona.edu>
2017-09-15 13:52:27 -0700

    More on t1.py

--- t1.py
+++ t1.py
@@ -1,11 +1,31 @@
 import subprocess
 import sys
 
+def find_python():
+    """Return a command that will run Python 3"""
+    for command in ["python3","python"]:
+        rc = subprocess.call([command, "--version"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        if rc == 0:
+            return command
+
+    print("OOPS! Can't figure out how to run Python 3!")
+
+def show_file(fname):
+    print("----- Contents of file '{}' -----".format(fname))
+    try:
+        f = open(fname, "r")
+        print(f.read(), end="")
+        f.close()
+    except Exception as e:
+        print(e)
+
 def main():
+    python_command = find_python()
     print(sys.version)
     infile = open("in.1","r")
     out = open("out.1","w")
-    rc = subprocess.call(["python3","test_seq.py"], stdin=infile, stdout=out, stderr=subprocess.STDOUT)
-    print("rc =", rc)   
+    rc = subprocess.call([python_command,"test_seq.py"], stdin=infile, stdout=out, stderr=subprocess.STDOUT)
+    print("rc =", rc)
+    show_file("out.1")
 
 main()
