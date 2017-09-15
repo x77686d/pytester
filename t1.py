75b3633b79d5ca1b10d2604983f9c4145c3f9dc1
William H. Mitchell <whm@cs.arizona.edu>
2017-09-15 14:58:26 -0700

    More on t1.py

--- t1.py
+++ t1.py
@@ -1,5 +1,11 @@
+"""
+Look for testing directory and if not present, populate it with current contents
+"""
 import subprocess
 import sys
+import urllib.request
+import re
+from pathlib import Path
 
 def find_python():
     """Return a command that will run Python 3"""
@@ -11,7 +17,45 @@ def find_python():
         except:
             pass
 
-    print("OOPS! Can't figure out how to run Python 3!")
+    print("Oops! Can't figure out how to run Python 3!")
+
+def build_test_dir(url):
+    """Build the test directory, if needed.
+        Cases:
+            Doesn't exist: make directory and populate it
+            Exists: do nothing
+            Exists but is a file: tell the user
+    """
+    test_dir = Path("test")
+    if test_dir.is_dir():
+        return
+
+    if test_dir.is_file():
+        print("""Oops!  The tester needs to create a directory named 'test'
+but you've got a file named 'test'.  Remove it and run me again.""")
+        sys.exit(1)
+
+    try:
+        test_dir.mkdir()
+    except Exception as e:
+        print("Oops!  Tried to create directory 'test' but failed with this:")
+        print(e)
+        sys.exit(1)
+    
+    with urllib.request.urlopen(url) as f:
+        s = f.read()
+        #print(s)
+        for m in re.finditer(r'>input-([0-9]+)\.txt<',str(s)):
+            testnum = m.group(1)
+            for fname in ["{}-{}.txt".format(name, testnum) for name in ["input","expected"]]:
+                print(fname)
+                with urllib.request.urlopen(url + "/" + fname) as testurl:
+                    testfilepath = test_dir / fname
+                    testfile = testfilepath.open(mode="wb")
+                    testfile.write(testurl.read())
+                    testfile.close()
+            
+
 
 def show_file(fname):
     print("----- Contents of file '{}' -----".format(fname))
@@ -31,4 +75,5 @@ def main():
     print("rc =", rc)
     show_file("out.1")
 
-main()
+#main()
+build_test_dir('http://www2.cs.arizona.edu/~whm/120/a3')
